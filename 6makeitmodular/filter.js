920128ed82b717580a47acf96ef1f248738e5960
Jhonbeltran <jbeltranleon@gmail.com>
2016-07-08 18:48:12 -0500

    7httpclient.js finished with GET

--- 6makeitmodular/filter.js
+++ 6makeitmodular/filter.js
@@ -2,7 +2,7 @@ var fs = require('fs') //uso file system para acceder a los archivos del disco
 var path = require('path') //path es usado para acceder a las rutas de los directorios
 
 module.exports = function (dir, ext, callback){ //funcion que vamos a exportar
-	fs.readdir(dir, function doneReading(err, list) { //.eaddir para leer un directorio
+	fs.readdir(dir, function doneReading(err, list) { //.readdir para leer un directorio
 		if(err) return callback(err)//si existe un error lo envia al callback
 
     	list = list.filter(function(file) {
